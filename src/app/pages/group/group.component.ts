cba439be84d552b799c44d574f2e80cafc8b825a
avocatsys <avocatsys@gmail.com>
2023-03-09 20:25:36 -0300

    ajuste dos grupos.

--- src/app/pages/group/group.component.ts
+++ src/app/pages/group/group.component.ts
@@ -1,138 +1,126 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { MessageService } from "primeng/api";
-import { Group } from "src/app/models/group.models";
-import { GroupService } from "src/app/services/group.service";
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ConfirmationService, MessageService } from 'primeng/api'
+import { Group } from 'src/app/models/group.models'
+import { GroupService } from 'src/app/services/group.service'
 
 @Component({
-  selector: "app-group",
-  templateUrl: "./group.component.html",
-  styles: [
-    `
-      :host ::ng-deep .p-message {
-        margin-left: 0.25em;
-      }
-
-      :host ::ng-deep .p-toast {
-        z-index: 99999;
-      }
-    `,
-  ],
-  providers: [MessageService, GroupService],
+    selector: 'app-group',
+    templateUrl: './group.component.html',
+    providers: [MessageService, GroupService, ConfirmationService],
 })
 export class GroupComponent implements OnInit {
-  rows: Group[];
+    rows: Group[]
 
-  navigation: string = "";
+    navigation: string = ''
 
-  busy: boolean = false;
+    busy: boolean = false
 
-  formGroup: FormGroup;
+    formGroup: FormGroup
 
-  constructor(
-    private fb: FormBuilder,
-    private service: MessageService,
-    private groupService: GroupService
-  ) {}
+    constructor(private fb: FormBuilder, private service: MessageService, private groupService: GroupService, private confirmationService: ConfirmationService) {}
 
-  ngOnInit(): void {
-    this.load();
+    ngOnInit(): void {
+        this.load()
 
-    this.formGroup = this.fb.group({
-      id: [null],
-      name: [
-        "",
-        Validators.compose([
-          Validators.required,
-          Validators.min(5),
-          Validators.max(250),
-        ]),
-      ],
-    });
-  }
-
-  save() {
-    this.busy = true;
-    this.formGroup.disable();
-    if (
-      this.formGroup.value.id == null ||
-      this.formGroup.value.id == ""
-    ) {
-      this.groupService.save(this.formGroup.value).subscribe({
-        next: () => {
-          this.showSuccessViaToast(),
-            this.formGroup.reset(),
-            (this.busy = false),
-            this.formGroup.enable();
-        },
-        error: () => {
-          this.showErrorViaToast(), (this.busy = false);
-          this.formGroup.enable();
-        },
-      });
-    } else {
-      this.update();
+        this.formGroup = this.fb.group({
+            id: [null],
+            name: ['', Validators.compose([Validators.required, Validators.min(5), Validators.max(250)])],
+        })
     }
-  }
 
-  load() {
-    this.groupService.load().subscribe({
-      next: (data) => {
-        this.rows = data.content;
-      },
-      error: () => {
-        this.showErrorViaToast();
-      },
-    });
-  }
+    save() {
+        this.busy = true
+        this.formGroup.disable()
+        if (this.formGroup.value.id == null || this.formGroup.value.id == '') {
+            this.groupService.save(this.formGroup.value).subscribe({
+                next: () => {
+                    this.showSuccessViaToast(), this.formGroup.reset(), (this.busy = false), this.formGroup.enable()
+                },
+                error: () => {
+                    this.showErrorViaToast(), (this.busy = false)
+                    this.formGroup.enable()
+                },
+            })
+        } else {
+            this.update()
+        }
+    }
 
-  edit(uuid: string) {
-    this.navigation = "form";
-    this.busy = true;
-    this.formGroup.enable();
-    this.groupService.findById(uuid).subscribe({
-      next: (data) => {
-        this.formGroup.patchValue(data);
-        this.busy = false;
-      },
-      error: () => {
-        this.showSuccessViaToast();
-        this.formGroup.enable();
-      },
-    });
-  }
+    load() {
+        this.groupService.load().subscribe({
+            next: (data) => {
+                this.rows = data.content
+            },
+            error: () => {
+                this.showErrorViaToast()
+            },
+        })
+    }
 
-  update() {
-    this.busy = true;
-    this.formGroup.disable();
-    this.groupService.update(this.formGroup.value).subscribe({
-      next: () => {
-        this.showSuccessViaToast(),
-          this.formGroup.reset(),
-          (this.busy = false),
-          this.formGroup.enable();
-      },
-      error: () => {
-        this.showErrorViaToast(), (this.busy = false);
-      },
-    });
-  }
+    edit(uuid: string) {
+        this.navigation = 'form'
+        this.busy = true
+        this.formGroup.enable()
+        this.groupService.findById(uuid).subscribe({
+            next: (data) => {
+                this.formGroup.patchValue(data)
+                this.busy = false
+            },
+            error: () => {
+                this.showSuccessViaToast()
+                this.formGroup.enable()
+            },
+        })
+    }
 
-  showErrorViaToast() {
-    this.service.add({
-      key: "tst",
-      severity: "error",
-      summary: "Mensagem de Erro",
-      detail: "Ocorreu algum erro!",
-    });
-  }
+    update() {
+        this.busy = true
+        this.formGroup.disable()
+        this.groupService.update(this.formGroup.value).subscribe({
+            next: () => {
+                this.showSuccessViaToast(), this.formGroup.reset(), (this.busy = false), this.formGroup.enable()
+            },
+            error: () => {
+                this.showErrorViaToast(), (this.busy = false)
+            },
+        })
+    }
 
-  showSuccessViaToast() {
-    this.service.add({
-      key: "tst",
-      severity: "success",
-      summary: "Mensagem de Sucesso",
-      detail: "Salvo com sucesso!",
-    });
-  }
+    delete(id: string) {
+        this.confirmationService.confirm({
+            key: 'confirm1',
+            message: 'Confirma essa exclusão?',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.busy = true
+                this.groupService.delete(id).subscribe({
+                    next: () => {
+                        this.showSuccessViaToast(), (this.busy = false), this.load()
+                    },
+                    error: () => {
+                        this.showErrorViaToast(), (this.busy = false)
+                    },
+                })
+            },
+        })
+    }
+
+    showErrorViaToast() {
+        this.service.add({
+            key: 'tst',
+            severity: 'error',
+            summary: 'Mensagem de Erro',
+            detail: 'Ocorreu algum erro!',
+        })
+    }
+
+    showSuccessViaToast() {
+        this.service.add({
+            key: 'tst',
+            severity: 'success',
+            summary: 'Mensagem de Sucesso',
+            detail: 'Salvo com sucesso!',
+        })
+    }
 }
--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -1,45 +1,35 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Group, PageableGroup } from "../models/group.models";
-import { Security } from "../utils/security.utils";
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Group, PageableGroup } from '../models/group.models'
+import { Security } from '../utils/security.utils'
 
 @Injectable({
-  providedIn: "root",
+    providedIn: 'root',
 })
 export class GroupService {
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  private url = "http://localhost:8080/avocat";
+    private url = 'http://localhost:8080/avocat'
 
-  private readonly branchOfficeId = Security.getBranchOfficeId();
+    private readonly branchOfficeId = Security.getBranchOfficeId()
 
-  save(data: Group) {
-    return this.http.post<Group>(
-      `${this.url}/v1/branch-office/${this.branchOfficeId}/groups`,
-      data,
-      { headers: Security.composeHeaders() }
-    );
-  }
+    save(data: Group) {
+        return this.http.post<Group>(`${this.url}/v1/branch-office/${this.branchOfficeId}/groups`, data, { headers: Security.composeHeaders() })
+    }
 
-  update(data: Group) {
-    return this.http.put<Group>(
-      `${this.url}/v1/branch-office/${this.branchOfficeId}/groups`,
-      data,
-      { headers: Security.composeHeaders() }
-    );
-  }
+    update(data: Group) {
+        return this.http.put<Group>(`${this.url}/v1/branch-office/${this.branchOfficeId}/groups`, data, { headers: Security.composeHeaders() })
+    }
 
-  load() {
-    return this.http.get<PageableGroup>(
-      `${this.url}/v1/branch-office/${this.branchOfficeId}/groups`,
-      { headers: Security.composeHeaders() }
-    );
-  }
+    load() {
+        return this.http.get<PageableGroup>(`${this.url}/v1/branch-office/${this.branchOfficeId}/groups`, { headers: Security.composeHeaders() })
+    }
 
-  findById(uuid: string) {
-    return this.http.get<Group>(
-      `${this.url}/v1/branch-office/${this.branchOfficeId}/groups/${uuid}`,
-      { headers: Security.composeHeaders() }
-    );
-  }
+    findById(uuid: string) {
+        return this.http.get<Group>(`${this.url}/v1/branch-office/${this.branchOfficeId}/groups/${uuid}`, { headers: Security.composeHeaders() })
+    }
+    
+    delete(uuid: string) {
+        return this.http.delete(`${this.url}/v1/branch-office/${this.branchOfficeId}/groups/${uuid}`, { headers: Security.composeHeaders() })
+    }
 }
