4143b293c2974d88fc640ff0afac2d1200e5e20a
avocatsys <avocatsys@gmail.com>
2023-03-10 20:56:12 -0300

    ajuste no checkbox de privilegios.

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,229 +1,200 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { MessageService } from "primeng/api";
-import { BranchOffice } from "src/app/models/branch-office.models";
-import { Group } from "src/app/models/group.models";
-import { Privilege } from "src/app/models/privilege.models";
-import { User } from "src/app/models/user.models";
-import { BranchOfficeService } from "src/app/services/branch-office.service";
-import { GroupService } from "src/app/services/group.service";
-import { PrivilegeService } from "src/app/services/privilege.service";
-import { UserService } from "src/app/services/user.service";
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { MessageService } from 'primeng/api'
+import { BranchOffice } from 'src/app/models/branch-office.models'
+import { Group } from 'src/app/models/group.models'
+import { Privilege } from 'src/app/models/privilege.models'
+import { User } from 'src/app/models/user.models'
+import { BranchOfficeService } from 'src/app/services/branch-office.service'
+import { GroupService } from 'src/app/services/group.service'
+import { PrivilegeService } from 'src/app/services/privilege.service'
+import { UserService } from 'src/app/services/user.service'
 
 @Component({
-  selector: "app-user",
-  templateUrl: "./user.component.html",
-  providers: [MessageService, PrivilegeService, BranchOfficeService],
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    providers: [MessageService, PrivilegeService, BranchOfficeService],
 })
 export class UserComponent implements OnInit {
-  navigation: string = "";
+    navigation: string = ''
 
-  busy: boolean = false;
+    busy: boolean = false
 
-  pageTitle = "Usuários";
+    pageTitle = 'Usuários'
 
-  rows: User[];
+    rows: User[]
 
-  valCheck: string[];
+    privilegesOpt: Privilege[]
 
-  privilegesOptions: Privilege[];
+    privilegesChecked: string[]
 
-  groupDrop: Group[];
+    privilegeArray = []
 
-  branchOfficeDrop: BranchOffice[];
+    privileges: string[] = []
 
-  formGroup: FormGroup;
+    groupDrop: Group[]
 
-  constructor(
-    private fb: FormBuilder,
-    private service: MessageService,
-    private userService: UserService,
-    private privilegeService: PrivilegeService,
-    private branchOfficeService: BranchOfficeService,
-    private groupService: GroupService
-  ) {}
+    branchOfficeDrop: BranchOffice[]
 
-  ngOnInit(): void {
-    this.load();
-    this.loadPrivileges();
-    this.loadBranchOffices();
-    this.loadGroups();
+    formGroup: FormGroup
 
-    this.formGroup = this.fb.group({
-      id: [null],
-      name: [
-        "",
-        Validators.compose([
-          Validators.required,
-          Validators.min(5),
-          Validators.max(250),
-        ]),
-      ],
-      username: [
-        "",
-        Validators.compose([
-          Validators.required,
-          Validators.min(5),
-          Validators.max(250),
-        ]),
-      ],
-      password: [null],
-      branchOffice: [
-        null,
-        Validators.compose([
-          Validators.required,
-          Validators.min(5),
-          Validators.max(250),
-        ]),
-      ],
-      group: [
-        null,
-        Validators.compose([
-          Validators.required,
-          Validators.min(5),
-          Validators.max(250),
-        ]),
-      ],
-      privileges: [
-        null,
-        Validators.compose([
-          Validators.required,
-          Validators.min(5),
-          Validators.max(250),
-        ]),
-      ],
-    });
-  }
+    constructor(
+        private fb: FormBuilder,
+        private service: MessageService,
+        private userService: UserService,
+        private privilegeService: PrivilegeService,
+        private branchOfficeService: BranchOfficeService,
+        private groupService: GroupService
+    ) {}
 
-  save() {
-    this.busy = true;
-    this.formGroup.disable();
-    if (this.formGroup.value.id == null || this.formGroup.value.id == "") {
-      let privilegeArray = [];
-      this.valCheck.map((i) => {
-        let privilege = this.getPrivileges(i);
-        privilegeArray.push(privilege);
-      });
+    ngOnInit(): void {
+        this.load()
+        this.loadPrivileges()
+        this.loadBranchOffices()
+        this.loadGroups()
 
-      this.formGroup.get("privileges").setValue(privilegeArray);
-      this.formGroup.get("password").setValue(this.getPasswordMock());
-
-      this.userService.save(this.formGroup.value).subscribe({
-        next: () => {
-          this.showSuccessViaToast(), this.clear();
-        },
-        error: () => {
-          this.showErrorViaToast(), (this.busy = false), this.clear();
-        },
-      });
-    } else {
-      this.update();
+        this.formGroup = this.fb.group({
+            id: [null],
+            name: ['', Validators.compose([Validators.required, Validators.min(5), Validators.max(250)])],
+            username: ['', Validators.compose([Validators.required, Validators.min(5), Validators.max(250)])],
+            password: [null],
+            branchOffice: [null, Validators.compose([Validators.required, Validators.min(5), Validators.max(250)])],
+            group: [null, Validators.compose([Validators.required, Validators.min(5), Validators.max(250)])],
+            privileges: [null, Validators.compose([Validators.required, Validators.min(5), Validators.max(250)])],
+        })
     }
-  }
 
-  load() {
-    this.userService.load().subscribe({
-      next: (data) => {
-        this.rows = data.content;
-      },
-      error: () => {
-        this.showErrorViaToast();
-      },
-    });
-  }
+    save() {
+        this.busy = true
+        this.formGroup.disable()
 
-  loadPrivileges() {
-    this.privilegeService.load().subscribe({
-      next: (data) => {
-        this.privilegesOptions = data.content;
-      },
-      error: () => {
-        this.showErrorViaToast();
-      },
-    });
-  }
+        this.privilegeArray = []
+        this.privilegesChecked.map((i) => {
+            let privilege = this.getPrivileges(i)
+            this.privilegeArray.push(privilege)
+        })
 
-  loadBranchOffices() {
-    this.branchOfficeService.load().subscribe({
-      next: (data) => {
-        this.branchOfficeDrop = data.content;
-      },
-      error: () => {
-        this.showErrorViaToast();
-      },
-    });
-  }
+        this.formGroup.get('privileges').setValue(this.privilegeArray)
 
-  loadGroups() {
-    this.groupService.load().subscribe({
-      next: (data) => {
-        this.groupDrop = data.content;
-      },
-      error: () => {
-        this.showErrorViaToast();
-      },
-    });
-  }
+        if (this.formGroup.value.id == null || this.formGroup.value.id == '') {
+            this.userService.save(this.formGroup.value).subscribe({
+                next: () => {
+                    this.showSuccessViaToast(), this.clear()
+                },
+                error: () => {
+                    this.showErrorViaToast(), (this.busy = false), this.clear()
+                },
+            })
+        } else {
+            this.update()
+        }
+    }
 
-  edit(uuid: string) {
-    this.navigation = "form";
-    this.formGroup.enable();
-    this.userService.findById(uuid).subscribe({
-      next: (data) => {
-        this.formGroup.patchValue(data);
-        this.privilegesOptions = data.privileges;        
-        this.busy = false;
-      },
-      error: () => {
-        this.showErrorViaToast();
-        this.busy = false;
-      },
-    });
-  }
+    load() {
+        this.userService.load().subscribe({
+            next: (data) => {
+                this.rows = data.content
+            },
+            error: () => {
+                this.showErrorViaToast()
+            },
+        })
+    }
 
-  update() {
-    this.busy = true;
-    this.formGroup.disable();
-    this.userService.update(this.formGroup.value).subscribe({
-      next: () => {
-        this.showSuccessViaToast(), this.clear();
-      },
-      error: () => {
-        this.showErrorViaToast(), (this.busy = false);
-      },
-    });
-  }
+    loadPrivileges() {
+        this.privilegeService.load().subscribe({
+            next: (data) => {
+                this.privilegesOpt = data.content
+            },
+            error: () => {
+                this.showErrorViaToast()
+            },
+        })
+    }
 
-  getPrivileges(id: string) {
-    return this.privilegesOptions.filter((i) => i.id == id)[0];
-  }
+    loadBranchOffices() {
+        this.branchOfficeService.load().subscribe({
+            next: (data) => {
+                this.branchOfficeDrop = data.content
+            },
+            error: () => {
+                this.showErrorViaToast()
+            },
+        })
+    }
 
-  getPasswordMock(): string {
-    return Math.random().toString(36).slice(-8);
-  }
+    loadGroups() {
+        this.groupService.load().subscribe({
+            next: (data) => {
+                this.groupDrop = data.content
+            },
+            error: () => {
+                this.showErrorViaToast()
+            },
+        })
+    }
 
-  clear() {
-    this.formGroup.reset();
-    this.busy = false;
-    this.formGroup.enable();
-    this.privilegesOptions = [];
-  }
+    edit(uuid: string) {
+        this.busy = true
+        this.navigation = 'form'
+        this.formGroup.enable()
+        this.userService.findById(uuid).subscribe({
+            next: (data) => {
+                this.formGroup.patchValue(data)
 
-  showErrorViaToast() {
-    this.service.add({
-      key: "tst",
-      severity: "error",
-      summary: "Mensagem de Erro",
-      detail: "Ocorreu algum erro!",
-    });
-  }
+                this.privileges = data.privileges.map((i) => i.id)
+                this.busy = false
+            },
+            error: () => {
+                this.showErrorViaToast()
+                this.busy = false
+            },
+        })
+    }
 
-  showSuccessViaToast() {
-    this.service.add({
-      key: "tst",
-      severity: "success",
-      summary: "Mensagem de Sucesso",
-      detail: "Salvo com sucesso!",
-    });
-  }
+    update() {
+        this.busy = true
+        this.formGroup.disable()
+        this.userService.update(this.formGroup.value).subscribe({
+            next: () => {
+                this.showSuccessViaToast(), this.clear()
+            },
+            error: () => {
+                this.showErrorViaToast(), (this.busy = false)
+            },
+        })
+    }
+
+    getPrivileges(id: string) {
+        return this.privilegesOpt.filter((i) => i.id == id)[0]
+    }
+
+    checkValue(privilege: any) {
+        this.privilegesChecked = privilege
+    }
+
+    clear() {
+        this.formGroup.reset()
+        this.busy = false
+        this.formGroup.enable()
+        this.privileges = []
+    }
+
+    showErrorViaToast() {
+        this.service.add({
+            key: 'tst',
+            severity: 'error',
+            summary: 'Mensagem de Erro',
+            detail: 'Ocorreu algum erro!',
+        })
+    }
+
+    showSuccessViaToast() {
+        this.service.add({
+            key: 'tst',
+            severity: 'success',
+            summary: 'Mensagem de Sucesso',
+            detail: 'Salvo com sucesso!',
+        })
+    }
 }
